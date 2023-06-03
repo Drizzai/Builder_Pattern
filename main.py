2ae5aec008bf03dfe92256b86d433ba33b48b291
Drizzai <96794735+Drizzai@users.noreply.github.com>
2023-06-03 19:43:16 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,46 @@
+from director import Director
+from sandwichbuilder import SandwichBuilder
+from soupbuilder import SoupBuilder
+from mainmealbuilder import MainMealBuilder
+from desertbuilder import DesertBuilder
+
+director = Director()
+
+builder = SoupBuilder()
+director.builder = builder
+
+builder.dish_part_a("Woda")
+builder.dish_part_b("Makaron")
+builder.dish_part_c("Przecier pomidorowy")
+builder.dish_part_d("Kostka rosołowa")
+director.build_dish()
+
+print("________________________________________________________________")
+
+builder = MainMealBuilder()
+director.builder = builder
+
+builder.dish_part_a("Kotlet schabowy")
+builder.dish_part_b("Frytki")
+builder.dish_part_c("Surówka")
+director.build_dish()
+
+print("________________________________________________________________")
+
+builder = SandwichBuilder()
+director.builder = builder
+
+builder.dish_part_a("Bułka pszenna")
+builder.dish_part_b("Szynka")
+builder.dish_part_c("Pomidor")
+director.build_dish()
+
+print("________________________________________________________________")
+
+builder = DesertBuilder()
+director.builder = builder
+
+builder.dish_part_a("Lody")
+builder.dish_part_b("Pistacjowe")
+builder.dish_part_b("Polewa czekoladowa")
+director.build_dish()
